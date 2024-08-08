5b214b13c1ac64a5c34fdd7ad53e521d41e8378f
Murat <muratgergov0820@gmail.com>
2024-08-09 01:10:01 +0300

    feat: Консольная команда для создания администратора

---
+++ app/Console/Commands/CreateAdminUserCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Enums\UserTypesEnum;
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class CreateAdminUserCommand extends Command
+{
+    protected $signature = 'app:create-admin-user-command';
+
+    protected $description = 'Создание главного администратора';
+
+    public function handle(): void
+    {
+        $admin = User::create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'type' => UserTypesEnum::admin,
+            'password' => '1',
+        ]);
+
+        if (is_null($admin)) {
+            $this->error(
+                'При создании администратора произошла ошибка, повторите попытку позднее'
+            );
+            return;
+        }
+
+        $this->info('Администратор успешно создан');
+    }
+}
---
+++ app/Console/Commands/PrepareProjectCommand.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class PrepareProjectCommand extends Command
+{
+    protected $signature = 'app:prepare-project';
+
+    protected $description = 'Подготовка необходимых сущностей/данных';
+
+    // TODO: Какая-та чанда с написанием месседжа в сообщении, разобраться
+    public function handle(): void
+    {
+        $commands = [
+            CreateAdminUserCommand::class
+        ];
+
+        /** @var Command $command */
+        foreach ($commands as $command) {
+            (new $command())->handle();
+        }
+
+        $this->info('Все команды успешно применены');
+    }
+}
